d641b2de785adcf945a5e660280aaaf93f6dc048
Joshua Anaya <j.anaya@outlook.com>
2022-08-11 11:51:05 -0700

    cpanel functionality required syntax corrections

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('message'))
-    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 5000)" x-show="show" class="fixed top-2 left-1/4 md:left-1/3 rounded transform-translate-x-1/2 bg-laravel text-white md:px-50 lg:px-48 py-5">
+    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" class="fixed top-2 left-1/4 md:left-1/3 rounded transform-translate-x-1/2 bg-laravel text-white md:px-50 lg:px-48 py-5">
         <p>
             {{session('message')}}
         </p>
